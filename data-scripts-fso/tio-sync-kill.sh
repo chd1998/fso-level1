fc80540fbdaa3f51c2979f0a38e2abc56ef31d63
chd1998 <chd1998@gmail.com>
2019-05-08 16:58:49 +0800

    revised method in getting pid

--- data-scripts-fso/tio-sync-kill.sh
+++ data-scripts-fso/tio-sync-kill.sh
@@ -6,43 +6,50 @@
 #example: none
 #changlog:
 #       20190427        release 0.1
-#       20190507        release 0.2	fixed some errors     
+#       20190507        release 0.2	fixed some errors
+#   	20190508        release 0.3     fixed method in get pid of process     
 
-echo "===== Welcome to Data Archiving System @FSO! ====="
-echo "           tio-sync-kill.sh Release 0.2           "
-echo "                  20190507 18:14                  "
-echo " "
 cyear=`date --date='0 days ago' +%Y`
 today=`date --date='0 days ago' +%Y%m%d`
-ctime=`date --date='0 days ago' +%H:%M:%S` 
+ctime=`date --date='0 days ago' +%H:%M:%S`
+
+echo "===== Welcome to Data Archiving System @FSO! ====="
+echo "           tio-sync-kill.sh Release 0.3           "
+echo " "
 p_name="tio-sync.sh"
 p_name1="wget"
-echo "Monitoring $p_name & $p_name1..."
-pid="$(pidof $p_name)"
+
+echo "$today $ctime: Monitoring $p_name & $p_name1..."
+#pid="$(pidof $p_name)"
+pid="$(ps x|grep -w tio-sync.sh|grep -v grep|awk '{print $1}'|head -n1)"
+#echo "$pid"
 pid1="$(pidof $p_name1)"
 runtime=0
 runtime1=0
+
 if [ -z $pid ];then
   echo "$p_name  process not found!"
-  if [ -z $pid1 ];then
-    echo "$p_name1 process not found!"
-    exit 0
-  else
-    runtime1=`ps -p $pid1 -o etime= | tr '-' ':' | awk -F: '{ total=0; m=1; } { for (i=0; i < NF; i++) {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }} {print total}'`
-  fi
 else
-   runtime=`ps -p $pid -o etime= | tr '-' ':' | awk -F: '{ total=0; m=1; } { for (i=0; i < NF; i++) {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }} {print total}'`
+  runtime=`ps -p $pid -o etime= | tr '-' ':' | awk -F: '{ total=0; m=1; } { for (i=0; i < NF; i++) {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }} {print total}'`
 fi
+
+if [ -z $pid1 ];then
+  echo "$p_name1 process not found!"
+else
+  runtime1=`ps -p $pid1 -o etime= | tr '-' ':' | awk -F: '{ total=0; m=1; } { for (i=0; i < NF; i++)  {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }} {print total}'`
+fi
+
 #echo "$runtime"
 #echo "$runtime1"
 #read
 echo "$today $ctime: "
-echo "                   $p_name has run for $runtime secs..."
-echo "                   $p_name1 has run for $runtime1 secs..."
+echo "                   $p_name($pid) has run for $runtime secs..."
+echo "                   $p_name1($pid1) has run for $runtime1 secs..."
 #echo "runtime: $runtime"
-#if time > 14400s , kill it 
+#when time > 14400s , kill it 
 today=`date --date='0 days ago' +%Y%m%d`
 ctime=`date --date='0 days ago' +%H:%M:%S`
+
 if [ $runtime -ge 14400 ];then
   kill $pid
   echo "$today $ctime: $p_name($pid) runs $runtime secs. "
