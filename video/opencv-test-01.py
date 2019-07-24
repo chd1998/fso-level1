453388fd379c5e9e5841bae419036651180c3ad9
chd1998 <chd1998@gmail.com>
2019-07-24 13:51:08 +0800

    beautifing with yapf

--- video/opencv-test-01.py
+++ video/opencv-test-01.py
@@ -1,14 +1,16 @@
 import numpy as np
 import cv2
 
+
 def change_res(width, height):
     cap.set(3, width)
     cap.set(4, height)
 
+
 cap = cv2.VideoCapture(0)
 change_res(1280, 720)
 
-while(True):
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -16,7 +18,7 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Display the resulting frame
 
-    cv2.imshow('Image from UVC Video --- press q to quit',gray)
+    cv2.imshow('Image from UVC Video --- press q to quit', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
