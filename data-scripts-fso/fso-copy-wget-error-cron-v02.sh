5393c904de43c4ef7b0031187d37ada58ef0739f
chd1998 <chd1998@gmail.com>
2019-07-24 23:23:11 +0800

    revised

--- data-scripts-fso/fso-copy-wget-error-cron-v02.sh
+++ data-scripts-fso/fso-copy-wget-error-cron-v02.sh
@@ -8,35 +8,34 @@
 
 #waiting pid taskname prompt
 waiting() {
-	local pid="$1"
-	taskname="$2"
-	procing "$3" &
-	local tmppid="$!"
-	wait $pid
-	tput rc
-	tput ed
-	ctime=`date --date='0 days ago' +%H:%M:%S`
-	today=`date --date='0 days ago' +%Y%m%d`
-	echo "$today $ctime: $2 Task Has Done!"
-#  echo "                   Finishing..."
-	kill -6 $tmppid >/dev/null 1>&2
+  local pid="$1"
+  taskname="$2"
+  procing "$3" &
+  local tmppid="$!"
+  wait $pid
+#  tput rc
+#  tput ed
+  ctime=`date --date='0 days ago' +%H:%M:%S`
+  today=`date --date='0 days ago' +%Y%m%d`
+  echo "$today $ctime: $2 Task Has Done!"
+  kill -6 $tmppid >/dev/null 1>&2
 }
 
 procing() {
-	trap 'exit 0;' 6
-	tput ed
-	while [ 1 ]
-	do
-		for j in '-' '\\' '|' '/'
-			do
-				tput sc
-				ptoday=`date --date='0 days ago' +%Y%m%d`
-				pctime=`date --date='0 days ago' +%H:%M:%S`
-				echo -ne  "$ptoday $pctime: $1...   $j"
-				sleep 1
-				tput rc
-			done
-	done
+  trap 'exit 0;' 6
+  tput ed
+  while [ 1 ]
+  do
+#    for j in '-' '\\' '|' '/'
+#    do
+#    tput sc
+    ptoday=`date --date='0 days ago' +%Y%m%d`
+    pctime=`date --date='0 days ago' +%H:%M:%S`
+    echo "$ptoday $pctime: $1, please wait... "
+    sleep 1
+#    tput rc
+#    done
+  done
 }
 
 trap 'onCtrlC' INT
