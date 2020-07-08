66f4bc50b61fe5f93fdce7f180d21f73d005aa50
chd1998 <chd1998@gmail.com>
2020-07-08 22:39:12 +0800

    rearrange the files

--- GFW-scripts/upgrade-trojan.sh
+++ GFW-scripts/upgrade-trojan.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Purposes: get latest release of trojan
-# Usage: upgrade-trojan.sh  owner project
-# Example: ./upgrade-trojan.sh trojan-gfw trojan
+# Usage: upgrade-trojan.sh  owner project ostype arch
+# Example: ./upgrade-trojan.sh trojan-gfw trojan linux amd64
 # NOTE: Press Ctrl+C to break
 #
 # Change History:
-#                20200704 20:50  Release 0.1: first working prototype
+#                20200704 20:50  Release 0.1 : first working prototype
+#                20200708 18:18  Release 0.2 : revised
 #
 cyear=`date  +%Y`
 today=`date  +%Y-%m-%d`
@@ -13,10 +14,10 @@ today0=`date  +%Y-%m-%d`
 ctime=`date  +%H:%M:%S`
 ctime0=`date  +%H:%M:%S`
 
-if [ $# -ne 2 ];then
-  echo "Usage: upgrade-trojan.sh  owner project"
-  echo "Example: ./upgrade-trojan.sh trojan-gfw trojan"
-  echo "press ctrl-c to break!"
+if [ $# -ne 4 ];then
+  echo "Usage: upgrade-trojan.sh  owner project ostype arch"
+  echo "Example: ./upgrade-trojan.sh trojan-gfw trojan linux amd64"
+#  echo "press ctrl-c to break!"
   exit 0
 fi
 echo "$today0 $ctime0 : Getting latest version..."
@@ -24,8 +25,11 @@ vname=`wget -qO- -t1 -T2 "https://api.github.com/repos/$1/$2/releases/latest"|gr
 vdigi=`echo ${vname}| cut -c 2-`
 #echo $vname
 #echo $vdigi
+
+pver=0.2
+
 echo "$today0 $ctime0 : Getting $vname trojan pre-built file..."
-durl="https://github.com/$1/$2/releases/download/$vname/trojan-$vdigi-linux-amd64.tar.xz"
+durl="https://github.com/$1/$2/releases/download/$vname/trojan-$vdigi-$3-$4.tar.xz"
 wget -q $durl &
 wait
 if [ $? -ne 0 ];then
@@ -37,20 +41,20 @@ fi
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
 echo "$today1 $ctime2 : Extracting  $vname binary files..."
-tar -xf ./trojan-$vdigi-linux-amd64.tar.xz
+tar -xf ./trojan-$vdigi-$3-$4.tar.xz
 cd ./trojan
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
-echo "$today1 $ctime2 : Stop trojan service..."
-systemctl stop trojan-gfw
+echo "$today1 $ctime2 : Stop $1 service..."
+systemctl stop $1
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
 echo "$today1 $ctime2 : Replace trojan binary file..."
 cp -f trojan /usr/bin/.
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
-echo "$today1 $ctime2 : Restart trojan service..."
-systemctl start trojan-gfw
+echo "$today1 $ctime2 : Restart $1 service..."
+systemctl start $1
 if [ $? -ne 0 ];then
   today2=`date  +%Y-%m-%d`
   ctime3=`date  +%H:%M:%S`
@@ -59,7 +63,7 @@ if [ $? -ne 0 ];then
   exit 1
 fi
 cd ..
-rm -f ./trojan-$vdigi-linux-amd64.tar.xz
+rm -f ./trojan-$vdigi-$3-$4.tar.xz
 rm -rf ./trojan
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
