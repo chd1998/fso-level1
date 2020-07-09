7d4ff417ea18a65708f03292137d6700e0aa7a18
chd1998 <chd1998@gmail.com>
2020-07-09 10:17:09 +0800

    output revised for upgrade-trojan.sh

--- GFW-scripts/upgrade-trojan.sh
+++ GFW-scripts/upgrade-trojan.sh
@@ -20,16 +20,15 @@ if [ $# -ne 4 ];then
 #  echo "press ctrl-c to break!"
   exit 0
 fi
-echo "$today0 $ctime0 : Getting latest version..."
+echo "$today0 $ctime0 : Getting latest version number..."
 vname=`wget -qO- -t1 -T2 "https://api.github.com/repos/$1/$2/releases/latest"|grep "tag_name" |sed -E 's/.*"([^"]+)".*/\1/'`
 vdigi=`echo ${vname}| cut -c 2-`
 #echo $vname
 #echo $vdigi
 
 pver=0.2
-
-echo "$today0 $ctime0 : Getting $vname trojan pre-built file..."
 durl="https://github.com/$1/$2/releases/download/$vname/trojan-$vdigi-$3-$4.tar.xz"
+echo "$today0 $ctime0 : Getting $durl..."
 wget -q $durl &
 wait
 if [ $? -ne 0 ];then
@@ -40,7 +39,7 @@ if [ $? -ne 0 ];then
 fi
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
-echo "$today1 $ctime2 : Extracting  $vname binary files..."
+echo "$today1 $ctime2 : Extracting  trojan-$vdigi-$3-$4.tar.xz"
 tar -xf ./trojan-$vdigi-$3-$4.tar.xz
 cd ./trojan
 today1=`date  +%Y-%m-%d`
@@ -49,7 +48,7 @@ echo "$today1 $ctime2 : Stop $1 service..."
 systemctl stop $1
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
-echo "$today1 $ctime2 : Replace trojan binary file..."
+echo "$today1 $ctime2 : Upgrading trojan to $vname..."
 cp -f trojan /usr/bin/.
 today1=`date  +%Y-%m-%d`
 ctime2=`date  +%H:%M:%S`
@@ -58,7 +57,7 @@ systemctl start $1
 if [ $? -ne 0 ];then
   today2=`date  +%Y-%m-%d`
   ctime3=`date  +%H:%M:%S`
-  echo "$today2 $ctime3: Failed in upgrading $vname trojan file..."
+  echo "$today2 $ctime3: Failed in upgrading $vname $3 $4 trojan file..."
   cd /home/chd
   exit 1
 fi
