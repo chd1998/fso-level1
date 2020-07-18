431a27e971dfbd6cc821bbdad2f832f9efbbcba0
chd1998 <chd1998@gmail.com>
2020-07-18 10:40:28 +0800

    revised

--- rpi-weather/check-fso-weather.sh
+++ rpi-weather/check-fso-weather.sh
@@ -65,6 +65,9 @@ pname=$3
 #Version
 pver=0.2
 
+datafile="/home/pi/Desktop/communication/latest.csv"
+datafile1="$src/$year/fso-weather-$day.csv"
+
 echo "*********************************************************************************************************************** "
 #prevent script from multiple running
 lockfile=/fso-cache/$(basename $0)-rpi.lock
@@ -82,26 +85,25 @@ else
 fi
 
 #check src dir and file
-if [ ! -f $src/$year/fso-weather-$day.csv ];then
-  echo "$day $ctime : $src/$year/fso-weather-$day.csv is not exist!"
+if [ ! -f $datafile1 ];then
+  echo "$day $ctime : $datafile1 is not exist!"
   exit 1
 fi
 #sleep input sec.  after started
 day=$(date "+%Y-%m-%d")
 ctime=$(date "+%H:%M:%S")
+File_Time1=`sudo stat -c %z  $datafile1`
 #echo "$day $ctime : Waiting for $delaytime secs. ....."
 sleep $delaytime  & 
 waiting "$!" "Waiting" "Waiting for Data" "$delaytime"
 #latest data file
-datafile="/home/pi/Desktop/communication/latest.csv"
-
 #check current time and  modification time of data file
 #if diff>=300s, kill python3  for fso-weather.py 
 Current_Timestamp=`date +%s`		# 获取当前时间的 Unix 时间戳
 #File_Modified_Time=`stat -c %Y  $src/$year/fso-weather-$day.csv`	# 获取文件修改时间unix时间戳
-File_Modified_Time=`sudo stat -c %Y  $datafile`
-File_Time=`sudo stat -c %z  $src/$year/fso-weather-$day.csv`
-Difftime=`echo "$Current_Timestamp $File_Modified_Time"| awk '{print($1-$2)}'`
+File_Modified_Time=`sudo stat -c %Y  $datafile` # 获取文件修改时间unix时间戳
+File_Time=`sudo stat -c %z  $datafile`
+Difftime=`echo "$Current_Timestamp $File_Modified_Time"| awk '{print($1-$2)}'` # 获取当前时间和文件修改时间的 Unix 时间戳时间差
 #difft=`echo "$Difftime $standtime"|awk '{print($1-$2)}'`
 #difft=`echo "$difft"|awk '{print sqrt($1*$1)}'`
 #Difftime=`expr ${Current_Timestamp} - ${File_Modified_Timestamp}`	# 获取当前时间和文件修改时间的 Unix 时间戳时间差
@@ -110,7 +112,8 @@ day=$(date "+%Y-%m-%d")
 ctime=$(date "+%H:%M:%S")
 if [ $Difftime -ge $standtime ];then	# 如果时间差大于输入时间，说明文件修改时间是在输入时间前，也就是最近输入时间内文件没有更新
   echo "$day $ctime : $datafile modified @ $File_Time"
-  echo "$day $ctime : Modification time span  is $Difftime sec. >= required $standtime sec. --- Check Failed!"
+  #echo "                     : $datafile1 modified @ $File_Time1"
+  echo "                     : Modification time span  is $Difftime sec. >= required $standtime sec. --- Check Failed!"
   cpid=`pidof $pname`
   if [ $? -ne 0 ];then
     echo "$day $ctime : Couldn't find pid of $pname..."
@@ -127,8 +130,9 @@ if [ $Difftime -ge $standtime ];then	# 如果时间差大于输入时间，说�
     fi
   fi
 else
-  echo "$day $ctime :  $datafile modified @ $File_Time"
-  echo "$day $ctime : Modification time span is $Difftime sec. < required $standtime sec. --- Check Passed!"    
+  echo "$day $ctime : $datafile modified @ $File_Time"
+  #echo "                    : $datafile1 modified @ $File_Time1"
+  echo "                    : Modification time span is $Difftime sec. < required $standtime sec. --- Check Passed!"    
 fi
 
 #copy new data 
