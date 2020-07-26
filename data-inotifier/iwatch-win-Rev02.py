3695824e8ad7da3549888edeff70a4db912c0b0f
chd1998 <chd1998@gmail.com>
2020-07-26 23:55:57 +0800

    revised

--- data-inotifier/iwatch-win-Rev02.py
+++ data-inotifier/iwatch-win-Rev02.py
@@ -3,11 +3,61 @@ import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+from datetime import datetime, timedelta
+import random
+import locale
 import fire
+from colorama import Fore,Back,Style
+from colorama import init
+import os
+import subprocess
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.last_modified = datetime.now()
+
+    def on_modified(self, event):
+        if datetime.now() - self.last_modified < timedelta(seconds=1):
+            return
+        else:
+            self.last_modified = datetime.now()
+        print(f'Event type: {event.event_type}  path : {event.src_path}')
+        print(event.is_directory) # This attribute is also available
+
+    def on_deleted(self, event):
+        if datetime.now() - self.last_modified < timedelta(seconds=1):
+            return
+        else:
+            self.last_modified = datetime.now()
+        print(f'Event type: {event.event_type}  path : {event.src_path}')
+        print(event.is_directory) # This attribute is also available
+
+    def on_created(self, event):
+        if datetime.now() - self.last_modified < timedelta(seconds=1):
+            return
+        else:
+            self.last_modified = datetime.now()
+        #cmd='start /b '+'robocopy '+event.src_path+' '+syncDes+' /S'
+        #syncAction=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        print(f'Event type: {event.event_type}  path : {event.src_path}')
+        print(event.is_directory) # This attribute is also available
+
+    def on_moved(self, event):
+        if datetime.now() - self.last_modified < timedelta(seconds=1):
+            return
+        else:
+            self.last_modified = datetime.now()
+        print(f'Event type: {event.event_type}  path : {event.src_path}')
+        print(event.is_directory) # This attribute is also available
+
 
 def pysync(syncSrc,syncDest):
+    init(autoreset=True)
+    print(Fore.YELLOW+"%s Waiting for changes...\n" %(time.ctime()),end='\r')
+    #popen=subprocess.Popen(listen,stdout=subprocess.PIPE)
     logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-    event_handler = LoggingEventHandler()
+    event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, syncSrc, recursive=True)
     observer.start()
