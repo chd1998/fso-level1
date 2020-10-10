235e7ce333449317878bcb1be6e493467c7ed8b6
chd1998 <chd1998@gmail.com>
2020-10-10 17:44:09 +0800

    sync updated

--- data-scripts-fso/fso-sync-lftp.sh
+++ data-scripts-fso/fso-sync-lftp.sh
@@ -87,18 +87,19 @@ srcnumber=/home/chd/log/$datatype-$today-$server1-filenumber.dat
 
 lockfile=/home/chd/log/$(basename $0)-$datatype-$today.lock
 
-if [ ! -f $filenumber ];then
-  echo "0">$filenumber
-fi
-if [ ! -f $filesize ];then 
-  echo "0">$filesize
-fi
-if [ ! -f $filenumber1 ];then
-  echo "0">$filenumber1
-fi
-if [ ! -f $filesize1 ];then
-  echo "0">$filesize1
-fi
+
+#if [ ! -f $filenumber ];then
+#  echo "0">$filenumber
+#fi
+#if [ ! -f $filesize ];then 
+#  echo "0">$filesize
+#fi
+#if [ ! -f $filenumber1 ];then
+#  echo "0">$filenumber1
+#fi
+#if [ ! -f $filesize1 ];then
+#  echo "0">$filesize1
+#fi
 
 if [ -f $lockfile ];then
   mypid=$(cat $lockfile)
@@ -108,9 +109,17 @@ if [ -f $lockfile ];then
     exit 1
   else
     echo $$>$lockfile
+    echo "0">$filenumber
+    echo "0">$filesize
+    echo "0">$filenumber1
+    echo "0">$filesize1
   fi
 else
   echo $$>$lockfile
+  echo "0">$filenumber
+  echo "0">$filesize
+  echo "0">$filenumber1
+  echo "0">$filesize1
 fi
 
 progversion=0.94
@@ -131,21 +140,13 @@ echo " "
 #procCmd=`ps ef|grep -w $procName|grep -v grep|wc -l`
 #pid=$(ps x|grep -w $procName|grep -v grep|awk '{print $1}')
 #if [ $procCmd -le 0 ];then
-if [ $datatype == "SP" ];then
-  destdir=${destpre0}${syssep}${cyear}${syssep}${today}${syssep}${datatype}
-else
-  destdir=${destpre0}${syssep}${cyear}${syssep}${today}${syssep}
-fi
 
+destdir=${destpre0}${syssep}${cyear}${syssep}${today}${syssep}
 targetdir=${destdir}${datatype}
 if [ ! -d "$targetdir" ]; then
   mkdir -m 777 -p $targetdir
 else
   echo "$today $ctime: $targetdir exists!"
-fi
-
-if [ $datatype == "SP" ];then
-  destdir=${destpre0}${syssep}${cyear}${syssep}${today}${syssep}${datatype}
 fi
 
 srcdir=${syssep}${today}${syssep}${datatype}
@@ -154,7 +155,7 @@ srcdir1=${srcpre0}
 
 n1=$(cat $filenumber)
 s1=$(cat $filesize)
-
+cd $targetdir
 ctime=`date  +%H:%M:%S`
 echo "$today $ctime: Syncing $datatype data @ FSO..."
 echo "             From: $server$srcdir "
@@ -181,7 +182,6 @@ else
   lftp  $server -e "mirror  -x '^\.' --parallel=$pnum  $srcdir0 $destdir; quit">/dev/null 2>&1 &
   #lftp -p 2121 -u tio,ynao246135 -e "mirror --only-missing --continue  --parallel=40  /20190704/TIO /lustre/data/2019/20190704/TIO; quit" ftp://192.168.111.120 >/dev/null 2>&1 &
   #wget  --tries=3 --timestamping --retry-connrefused --timeout=10 --continue --inet4-only --ftp-user=tio --ftp-password=ynao246135 --no-host-directories --recursive  --level=0 --no-passive-ftp --no-glob --preserve-permissions $srcdir1
-
   waiting "$!" "$datatype Syncing" "Syncing $datatype Data"
   ctime3=`date  +%H:%M:%S`
   if [ $? -ne 0 ];then
@@ -192,22 +192,6 @@ else
   ctime2=`date  +%H:%M:%S`
 
   syncend=`date +%s`
-  #mytime2=$(cat /home/chd/log/$(basename $0)-$datatype-sdtmp.dat)
-  #mytime2=`echo $ttmp|tr '-' ':' | awk -F: '{ total=0; m=1; } { for (i=0; i < NF; i++) {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }} {print total}'`
-
-  #curhm=`date  +%H%M`
-  #if [ $curhm -ge 1501 ]; then
-  #chmod 777 -R $targetdir &
-  #find $targetdir ! -perm 777 -type f -exec chmod 777 {} \; & 
-  #find $targetdir ! -perm 777 -type d -exec chmod 777 {} \; &
-  #  waiting "$!" "$datatype Files Permission Changing" "Changing $datatype Files Permission"
-  #  if [ $? -ne 0 ];then
-  #    ctime3=`date  +%H:%M:%S`
-  #    echo "$today $ctime3: Changing Permission of $datatype Failed!"
-  #    cd /home/chd/
-  #    exit 1
-  #  fi
-  #fi
 fi
   
 ctime2=`date  +%H:%M:%S`
@@ -215,9 +199,9 @@ echo "$today $ctime2: Summerizing $datatype File Numbers & Size..."
 #n2=`ls -lR $targetdir | grep "^-" | wc -l` 
 #s2=`du -sm $targetdir|awk '{print $1}'` 
 
-filetmp=${target}${syssep}*.fits
-
-find $targetdir | grep fits | wc -l > $filenumber1 &
+#filetmp=${target}${syssep}*.fits
+cd $targetdir
+find $targetdir -name *.fits -type f | wc -l > $filenumber1 &
 waiting "$!" "$datatype File Number Sumerizing" "Sumerizing $datatype File Number"
 if [ $? -ne 0 ];then
   ctime3=`date  +%H:%M:%S`
@@ -225,8 +209,7 @@ if [ $? -ne 0 ];then
   cd /home/chd/
   exit 1
 fi
-
-du -sm $targetdir|awk '{print $1}' > $filesize1 &
+du -sm $targetdir/|awk '{print $1}' > $filesize1 &
 //cd $targetdir
 //find $targetdir -name *.fits -type f | xargs -I {} ls -al|awk '{sum += $5} END {print sum/(1024*1024)}' > $filesize1 &
 waiting "$!" "$datatype File Size Summerizing" "Sumerizing $datatype File Size"
@@ -237,10 +220,10 @@ if [ $? -ne 0 ];then
   exit 1
 fi
 
-if [ ! -d "$targetdir" ]; then
-  echo "0" > $filesize1
-  echo "0" > $filenumber1
-fi
+#if [ ! -d "$targetdir" ]; then
+#  echo "0" > $filesize1
+#  echo "0" > $filenumber1
+#fi
 
 find $targetdir ! -perm 777 -type d -exec chmod 777 {} \; &
 
@@ -251,11 +234,13 @@ sn=`echo "$n1 $n2"|awk '{print($2-$1)}'`
 ss=`echo "$s1 $s2"|awk '{print($2-$1)}'`
 
 synctime=`echo "$syncstart $syncend"|awk '{print($2-$1)}'`
-if [ $synctime -le 0 ]; then
-	synctime=1
+if [ $synctime -le 0 ];then
+  synctime=0
+  speed=0
   ss=0
+else
+  speed=`echo "$ss $synctime"|awk '{print($1/$2)}'`
 fi
-speed=`echo "$ss $synctime"|awk '{print($1/$2)}'`
 
 echo $n2>$filenumber
 echo $s2>$filesize
@@ -269,6 +254,7 @@ else
   srcday=$today
   srctime=$ctime2
 fi
+
 if [ -z $srcs ];then
   srcs=0
 fi
