48e51e6d06e3f777fefa9334b09e132aa9c4c603
chd1998 <chd1998@gmail.com>
2020-11-04 11:14:27 +0800

    Merge branch 'master' of https://github.com/chd1998/python-fso into master

--- data-scripts-fso/data-sum-daily.sh
+++ data-scripts-fso/data-sum-daily.sh
@@ -28,22 +28,24 @@ datatype=$4
 mailornot=$5
 
 pver=0.1.2
-num=0
-size=0.0
-interval=0.0
-start="null"
-end="null"
+num="00000000"
+size="0000000.0000"
+interval="0000.000000"
+start="0000-00-00 00:00:00.000000000"
+end="0000-00-00 00:00:00.000000000"
+
 homepre=/home/chd/data-info
 targetdir=$progpre/$year/$year$monthday/$datatype
 sumdir=$homepre/$year
 suminfo=$sumdir/$datatype-$year-$monthday.sum
 
 device="lustre"
+site="fso"
 dataprefix=`echo $datatype|echo ${datatype:0:1}`
 t0=`date  +%Y%m%d`
 d0=`date +%H:%M:%S`
 dt0=`date +%s`
-echo "$today0 $ctime : Start  $year$monthday $datatype Data @$device Summerizing, Pls Wait..."
+#echo "$today0 $ctime : Start  $year$monthday $datatype Data @$device Summerizing, Pls Wait..."
 if [ ! -d "$sumdir" ]; then
   mkdir -m 777 -p $sumdir
 fi
@@ -52,45 +54,71 @@ if [ -d "$targetdir" ]; then
   today0=`date  +%Y%m%d`
   ctime=`date  +%H:%M:%S`
   cd $targetdir
-  #echo "$today0 $ctime : Start Counting $year$monthday $datatype @$device File Numbers & Size..."
+  if [ $mailornot -eq "1" ];then
+    echo "$today0 $ctime : Start Counting $year$monthday $datatype @$device File Numbers & Size..."
+  fi 
   num=`find ./ -name $dataprefix*.fits -type f | wc -l`
   if [ $num -gt "0" ];then
     size=`find ./ -name $dataprefix*.fits -type f | xargs ls -I {} -al|awk '{sum += $5} END {print sum/(1000*1024*1024)}'` 
+    num=`printf "%08d" $num`
+    size=`printf "%012.4f" $size`
+  else
+    num="00000000"
+    size="0000000.0000"
+    interval="0000.000000"
   fi
   today0=`date  +%Y%m%d`
   ctime=`date  +%H:%M:%S`
   #echo "$today0 $ctime : Start Calculating  $year$monthday $datatype @$device Observing Time..."
   cd $targetdir
-  #find ./   -path "*redata*" -o -path "*dark*" -o -path "*FLAT*"  -prune -o -type f -name "$dataprefix*.fits" -print >$datatype-$year-$monthday-flist
-  #sort $datatype-$year-$monthday-flist>$datatype-$year-$monthday-flist-sorted
-  #start=`head -n +1 $datatype-$year-$monthday-flist-sorted | xargs stat |grep Change|awk '{print $2 " " $3}'`
-  #end=`tail -n -1 $datatype-$year-$monthday-flist-sorted | xargs stat |grep Change|awk '{print $2 " " $3}'`
-  #get earliest file's time
-  start=`find ./   -path "*redata*" -o -path "*dark*" -o -path "*FLAT*"  -prune -o -type f -name "$dataprefix*.fits" -print |xargs ls -ltr 2>/dev/null|head -n +1|awk '{print($9)}'|xargs stat|grep Change|awk '{print( $2" "$3)}'`
-  #get latest file's time
-  end=`find ./  -path "*redata*" -o -path "*dark*" -o -path "*FLAT*"  -prune -o -type f -name "$dataprefix*.fits" -print |xargs ls -lt 2>/dev/null|head -n +1|awk '{print($9)}'|xargs stat|grep Change|awk '{print( $2" "$3)}'`
-  s=`date -d "$start" +%s`
-  e=`date -d "$end" +%s`
+  start=`find ./   -path "*redata*" -o -path "*Dark*" -o -path "*dark*" -o -path "*FLAT*"  -prune -o -type f -name $dataprefix*.fits -print |xargs ls -ltr 2>/dev/null |head -n +1|awk '{print($9)}'|xargs stat 2>/dev/null|grep Change|awk '{print($2" "$3)}'`
+  if [ -z "$start" ]; then
+    #start="19700101 08:00:00.000"
+    #s=`date -d "$start" +%s`
+    s=0
+    start="0000-00-00 00:00:00.000000000"
+  else
+    s=`date -d "$start" +%s`
+  fi
+  end=`find ./  -path "*redata*" -o -path "*Dark*" -o -path "*dark*" -o -path "*FLAT*"  -prune -o -type f -name $dataprefix*.fits -print |xargs ls -lt 2>/dev/null|head -n +1|awk '{print($9)}'|xargs stat 2>/dev/null|grep Change|awk '{print( $2" "$3)}'`
+  if [ -z "$end" ]; then
+    #end="19700101 08:00:00.000"
+    #e=`date -d "$end" +%s`
+    e=0
+    end="0000-00-00 00:00:00.000000000"
+  else
+    e=`date -d "$end" +%s`
+  fi
+  
   interval=`echo "$s $e"|awk '{print(($2-$1)/3600)}'`
+  interval=`printf "%011.6f" $interval`
 
   today0=`date  +%Y%m%d`
   ctime=`date  +%H:%M:%S`
-  echo "$year$monthday   $num             $size         $start              $end               $interval" > $suminfo
+  echo "$year$monthday   $num              $size               $start                       $end               $interval " > $suminfo
   if [ $mailornot -eq "1" ];then 
     echo "$today0 $ctime : Send Summary  for $year$monthday $datatype @$device to Users..."
-    mail -s "Summary of $year$monthday $datatype @$device" nvst_obs@ynao.ac.cn < $suminfo
-    mail -s "Summary of $year$monthday $datatype @$device" chd@ynao.ac.cn < $suminfo
+    echo "                      $datatype Data Summary $year$monthday @fso                                  "> ./$datatype-mailtmp
+    echo "Date       Nums.                 Size(GiB)                  StartTime                                           EndTime                                     Obs. Time(hrs)" >>./$datatype-mailtmp
+    echo "**********************************************************************************************************************************************************************************">> ./$datatype-mailtmp
+    cat $suminfo >> ./$datatype-mailtmp
+    mail -s "Summary of $year$monthday $datatype @$device" nvst_obs@ynao.ac.cn < ./$datatype-mailtmp
+    mail -s "Summary of $year$monthday $datatype @$device" chd@ynao.ac.cn < ./$datatype-mailtmp
   fi
   rm -f $datatype-$year-$monthday-flist
   rm -f $datatype-$year-$monthday-flist-sorted
+  rm -f ./$datatype-mailtmp
   today0=`date  +%Y%m%d`
   ctime=`date  +%H:%M:%S`
   t1=`date  +%Y%m%d`
   d1=`date +%H:%M:%S`
   dt1=`date +%s`
   dt=`echo $dt0 $dt1|awk '{print($2-$1)'}`
-  echo "$today0 $ctime : All Summary Tasks for $year$monthday $datatype @$device Ended..."
-  echo "               in : $dt secs."
+  #sleep 1
+  if [ $mailornot -eq "1" ];then
+    echo "$today0 $ctime : All Summary Tasks for $year$monthday $datatype @$device Ended..."
+    echo "               in : $dt secs."
+  fi
 else
   today0=`date  +%Y%m%d`
   ctime=`date  +%H:%M:%S`
